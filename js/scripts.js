0e3ee0f1e61d264d9ac79873dea71c8e1c8b9383
Curtis Martin <curtbusiness@gmail.com>
2021-05-31 23:03:11 -0500

    v2

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+
+
+
+
+// START fade on scroll
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    //document.getElementById(".jumbotron").style.fontSize = "30px";
+  } else {
+   //document.getElementById(".jumbotron").style.fontSize = "90px";
+  }
+}
+// END fade on scroll
